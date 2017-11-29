29916761e36d1eff1d5ca91ce45c50ab3fd90330
martapz <marta.g.mattioli@gmail.com>
2017-11-29 11:00:53 +0000

    added backend

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(port, () => console.log(`Express is listening on port ${port}`));
